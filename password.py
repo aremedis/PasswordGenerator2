01fefa5858adb2536b357bc76c23b7ca57360a12
Aremedis <aremedis@gmail.com>
2015-02-22 20:49:33 -0600

    error correction
    fixed the IF statement to actually work, when no argument is passed via
    command line.

--- password.py
+++ password.py
@@ -2,9 +2,16 @@ import random
 import string
 import sys
 
+# print (len(sys.argv))
 
-if sys.argv :
-	rng = int(sys.argv[1])
-else: rng = 34
+if len(sys.argv) < 2 :
+	print ("Given Length is too short, setting password length to 34")
+	rng = 34
+else: rng = int(sys.argv[1])
 
 print (''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(rng)]))
+
+
+# Things to add in the future:
+# -a way to exclude certain characters
+# -a way to check for duplicate characters, and allow for no duplicates
